b9d0e87db46755c984f0e6ac35002b64d1902344
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-03-28 11:48:06 +0900

    APIをRiverpodやfreezedを使用して呼び出し、返答を出力するまで 試しに記載。
    QiitaのAPIではないが、試しに実装しやすそうな内容にしているため
    developのみにプルリクを提出する。
    
    ■修正内容
    ・freezedとriverpod、dioのインストール
    ・APIで使用するデータをfreezedでmodelに作成
    ・API呼び出しと返答を出力

--- lib/view/show_search_result_page.dart
+++ lib/view/show_search_result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_application/ripository.dart';
 
 class ShowSearchResult extends StatefulWidget {
   const ShowSearchResult({Key? key}) : super(key: key);
@@ -53,6 +54,7 @@ class _ShowSearchResultState extends State<ShowSearchResult> {
         ),
         onTap: () {
           print("$titleがTapされました");
+          fetchParsonDataList();
         },
         onLongPress: () {
           // 長押し時の処理
