583d93fccca8d27fe72c5e226b602ce43759f1b6
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-04-07 10:18:48 +0900

    ■修正内容 各画面の文言を別ファイルで管理するよう変更

---
+++ lib/view/show_search_result/show_search_result_strings.dart
@@ -0,0 +1,17 @@
+/*
+ * show_search_resultで使用する文言をここで管理する
+ */
+/// 画面タイトル
+const String screenTitle = "Qiita検索表示";
+
+/// エラーログ
+const String errorString = "Error";
+
+/// 人気記事タブタイトル
+const String popularTabTitle = "人気記事";
+
+/// 記事検索タブタイトル
+const String searchArticleTabTitle = "記事検索";
+
+/// 設定タブタイトル
+const String settingTabTitle = "設定";
