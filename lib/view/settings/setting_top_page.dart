d0d66d47eb9203d8aa993c6bcd98412a2ed8552f
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-04-10 17:24:10 +0900

    ■修正内容 ・設定画面の変更 ・設定画面の要素を追加 ・「自分の記事を開く」を押下すると、Qiitaのマイページへ飛ぶ

--- lib/view/settings/setting_top_page.dart
+++ lib/view/settings/setting_top_page.dart
@@ -1,54 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qiita_application/view/settings/setting_top_strings.dart';
+import 'package:qiita_application/view_model/setting/setting_top_notifier.dart';
 
 class SettingTopPage extends StatelessWidget {
-  SettingTopPage({Key? key}) : super(key: key);
+  const SettingTopPage({Key? key}) : super(key: key);
 
   /// タブの高さ
   static const double tabBarHeight = 10;
 
-  final settingItem = [];
+  /// 間の大きさ
+  static const double height = 16;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: ListView.builder(
-          itemCount: 1,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (BuildContext context, int index) {
-            return showSettingItem(context, 'title');
-          },
-        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: const <Widget>[
+              /// ヘッダー
+              SettingHeadLine(title: settingHeadLine),
 
-        // child: Padding(
-        //   padding: const EdgeInsets.all(8.0),
-        //   child: ListView(
-        //     shrinkWrap: true,
-        //     physics: const NeverScrollableScrollPhysics(),
-        //     children: const <Widget>[
-        //       Card(
-        //         child: ListTile(
-        //           title: Text('title'),
-        //         ),
-        //       ),
-        //     ],
-        //     // child: Column(
-        //     //   children: const [
-        //     //     SizedBox(height: 10),
-        //     //     SettingHeadLine(title: 'title'),
-        //     //   ],
-        //     // ),
-        //   ),
-        // ),
+              /// バージョン情報
+              SettingVersion(title: settingVersion),
+              SizedBox(height: height),
+
+              /// UI設定
+              SettingHeadLine(title: headerUISetting),
+              SizedBox(height: height),
+              // テーマの切り替え
+              SettingVersion(title: titleTheme),
+              SizedBox(height: height),
+              // 記事サイズの変更
+              SettingVersion(title: titleStrSize),
+              SizedBox(height: height),
+
+              /// その他の設定
+              SettingHeadLine(title: headerSetting),
+              SizedBox(height: height),
+              // 自分の記事を開く
+              OpenMyPage(title: titleOpenMyPage),
+              // 規約
+              SettingVersion(title: titleRule),
+              // お知らせ
+              SettingVersion(title: titleInformation),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
 
-  ///
-  /// 設定をそれぞれ表示
-  ///
-  Widget showSettingItem(context, title) {
+/// アプリのバージョンを示す
+class SettingVersion extends StatelessWidget {
+  const SettingVersion({Key? key, required this.title}) : super(key: key);
+  final String title;
+  final String appVer = '1.0.0';
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
       child: Container(
         padding: const EdgeInsets.all(1.5),
@@ -56,21 +71,27 @@ class SettingTopPage extends StatelessWidget {
           border: Border(
             bottom: BorderSide(
               color: Colors.grey,
-              width: 1.5,
+              width: 1.2,
             ),
           ),
         ),
         child: ListTile(
+          leading: const Icon(Icons.bolt),
+          trailing: Text(
+            appVer,
+            style: const TextStyle(
+              color: Colors.teal,
+              fontSize: 16,
+            ),
+          ),
           title: Text(
             title,
             style: const TextStyle(
               color: Colors.black,
-              fontSize: 18,
+              fontSize: 16,
+              fontWeight: FontWeight.w100,
             ),
           ),
-          onTap: () {
-            print('設定押下');
-          },
         ),
       ),
     );
@@ -88,6 +109,46 @@ class SettingHeadLine extends StatelessWidget {
     return Align(
       alignment: Alignment.centerLeft,
       child: Text(title),
+    );
+  }
+}
+
+/// 自分の記事を開く
+class OpenMyPage extends ConsumerWidget {
+  const OpenMyPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  /// 自分の記事サンプルurl
+  static const String stringUri = 'https://qiita.com/railgun-0402';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      child: Container(
+        padding: const EdgeInsets.all(1.5),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 1.5,
+            ),
+          ),
+        ),
+        child: ListTile(
+          leading: const Icon(Icons.auto_stories),
+          title: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+          onTap: () {
+            openQiitaPage(stringUri);
+          },
+        ),
+      ),
     );
   }
 }
--- lib/view/settings/setting_top_strings.dart
+++ lib/view/settings/setting_top_strings.dart
@@ -1,3 +1,29 @@
 /*
  * setting_topで使用する文言をここで管理する
  */
+/// 設定のヘッダー
+const String settingHeadLine = 'アクション';
+
+/// バージョン
+const String settingVersion = 'バージョン情報';
+
+/// UI設定ヘッダー
+const String headerUISetting = 'UI設定';
+
+/// タイトル-テーマの切り替え
+const String titleTheme = 'テーマの切り替え';
+
+/// タイトル-記事の文字サイズを切り替え
+const String titleStrSize = '記事の文字サイズ';
+
+/// タイトル-自分の記事を開く
+const String titleOpenMyPage = '自分の記事を開く';
+
+/// タイトル-規約
+const String titleRule = '規約';
+
+/// タイトル-規約
+const String titleInformation = 'お知らせ';
+
+/// その他設定ヘッダー
+const String headerSetting = 'その他の設定';
